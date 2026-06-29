69f8afbc54eff736d7ed4dafcda36f71718dc762
AP23110010533 <karthikeya_vadlamudi@srmap.edu.in>
2026-06-29 10:48:21 +0530

    Updated Tip Calculator project

--- code.js
+++ code.js
@@ -1,20 +1,23 @@
-let a = document.getElementById("billAmount");
-let b = document.getElementById("percentageTip");
-let c = document.getElementById("errorMessage");
-let errorMessage = "Please Enter a Valid Input.";
-
 function calculateButton() {
-    if (a.value === "") {
-        c.textContent = errorMessage;
-    } else if (b.value === "") {
-        c.textContent = errorMessage;
-    } else {
-        c.textContent = "";
-        let a1 = parseInt(a.value);
-        let b1 = parseInt(b.value);
-        let C = (b1 / 100) * a1;
-        let d = a1 + C;
-        document.getElementById("tipAmount").value = C;
-        document.getElementById("totalAmount").value = d;
-    }
+  let a = document.getElementById("billAmount");
+  let b = document.getElementById("percentageTip");
+  let c = document.getElementById("errorMessage");
+
+  let errorMessage = "Please Enter a Valid Input.";
+
+  if (a.value === "") {
+    c.textContent = errorMessage;
+  } else if (b.value === "") {
+    c.textContent = errorMessage;
+  } else {
+    c.textContent = "";
+    let a1 = parseFloat(a.value);
+    let b1 = parseFloat(b.value);
+
+    let tip = (b1 / 100) * a1;
+    let total = a1 + tip;
+
+    document.getElementById("tipAmount").value = tip.toFixed(2);
+    document.getElementById("totalAmount").value = total.toFixed(2);
+  }
 }
